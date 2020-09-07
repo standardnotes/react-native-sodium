0322834a0b94736137b0b1564a7d988f7c6c1578
Radek Czemerys <radko93@gmail.com>
2020-09-07 17:04:32 +0200

    chore: ndk update

--- android/.cxx/cmake/debug/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
+++ android/.cxx/cmake/debug/x86/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -1,9 +1,9 @@
-set(CMAKE_HOST_SYSTEM "Darwin-19.5.0")
+set(CMAKE_HOST_SYSTEM "Darwin-19.6.0")
 set(CMAKE_HOST_SYSTEM_NAME "Darwin")
-set(CMAKE_HOST_SYSTEM_VERSION "19.5.0")
+set(CMAKE_HOST_SYSTEM_VERSION "19.6.0")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
-include("/Users/radko93/Library/Android/sdk/ndk/19.2.5345600/build/cmake/android.toolchain.cmake")
+include("/Users/radko93/Library/Android/sdk/ndk/21.3.6528147/build/cmake/android.toolchain.cmake")
 
 set(CMAKE_SYSTEM "Android-1")
 set(CMAKE_SYSTEM_NAME "Android")
