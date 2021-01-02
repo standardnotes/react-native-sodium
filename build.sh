8b2a1dfad5cd7ecf5e34bbe77d4d7127b3670bea
Radek Czemerys <radko93@gmail.com>
2021-01-02 12:24:10 +0100

    feature: xcframework for iOS

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@
 
 sigfile=`ls -1 libsodium-*-stable.tar.gz.minisig`
 srcfile=`basename $sigfile .minisig`
-echo $srcfile
 srcdir='libsodium-stable'
-echo $srcdir
 
 # --------------------------
 # Download and verify source
 # --------------------------
 [ -f $srcfile ] && rm -f $srcfile
 curl https://download.libsodium.org/libsodium/releases/$srcfile > $srcfile
-# gpg --no-default-keyring --keyring `pwd`/trusted.gpg --verify $sigfile $srcfile || exit 1
+minisign -P "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3" -Vm $srcfile || exit 1
 
 # --------------------------
 # Extract sources
@@ -30,29 +28,29 @@ do
   # --------------------------
   platform=`uname`
   if [ "$platform" == 'Darwin' ] && [ "$targetPlatform" == 'ios' ]; then
-    IOS_VERSION_MIN=9.0 dist-build/ios.sh
+    IOS_VERSION_MIN=10.0.0 dist-build/apple-xcframework.sh
   fi
 
   # --------------------------
   # Android build
   # --------------------------
-  # case $targetPlatform in
-  #   "arm-old")
-  #     NDK_PLATFORM=android-21 dist-build/android-arm.sh
-  #     ;;
-  #   "arm")
-  #     NDK_PLATFORM=android-21 dist-build/android-armv7-a.sh
-  #     NDK_PLATFORM=android-21 dist-build/android-armv8-a.sh
-  #     ;;
-  #   "mips")
-  #     NDK_PLATFORM=android-21 dist-build/android-mips32.sh
-  #     NDK_PLATFORM=android-21 dist-build/android-mips64.sh
-  #     ;;
-  #   "x86")
-  #     NDK_PLATFORM=android-21 dist-build/android-x86.sh
-  #     NDK_PLATFORM=android-21 dist-build/android-x86_64.sh
-  #   ;;
-  # esac
+  case $targetPlatform in
+    "arm-old")
+      NDK_PLATFORM=android-21 dist-build/android-arm.sh
+      ;;
+    "arm")
+      NDK_PLATFORM=android-21 dist-build/android-armv7-a.sh
+      NDK_PLATFORM=android-21 dist-build/android-armv8-a.sh
+      ;;
+    "mips")
+      NDK_PLATFORM=android-21 dist-build/android-mips32.sh
+      NDK_PLATFORM=android-21 dist-build/android-mips64.sh
+      ;;
+    "x86")
+      NDK_PLATFORM=android-21 dist-build/android-x86.sh
+      NDK_PLATFORM=android-21 dist-build/android-x86_64.sh
+    ;;
+  esac
 
 done
 cd ..
@@ -69,8 +67,9 @@ do
   mv $dir libsodium/
 done
 
-if [ "$platform" == 'Darwin' ] && [ -e $srcdir/libsodium-ios ]; then
-  mv $srcdir/libsodium-ios libsodium/
+if [ "$platform" == 'Darwin' ] && [ -e $srcdir/libsodium-apple ]; then
+  echo $PWD
+  mv $srcdir/libsodium-apple libsodium/
 fi
 
 # --------------------------
